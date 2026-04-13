1830bcce84559820c2b563614500fa55caf2bc7e
GitHub Yaocenji <104616890+Yaocenji@users.noreply.github.com>
2026-04-14 00:47:39 +0800

    【例行】4月13日晚例行提交

--- Assets/Prefabs/场景/Door1.cs
+++ Assets/Prefabs/场景/Door1.cs
@@ -6,15 +6,19 @@ namespace ProjectII.SceneItems
 {
     public class Door1 : InteractableBase
     {
-        public GameObject door;
-
         public float closeAngle = 0;
         public float openAngle = 90;
-        private float targetAngle;
+        
+        private HingeJoint2D hinge2D;
+        
+        private bool isOpen = false;
+
+        private float beginAngle;
 
         private void Start()
         {
-            targetAngle = closeAngle;
+            hinge2D = GetComponent<HingeJoint2D>();
+            beginAngle = transform.eulerAngles.z;
         }
 
         private void OnTriggerEnter2D(Collider2D other)
@@ -39,7 +43,31 @@ namespace ProjectII.SceneItems
         protected override void OnInteract()
         {
             Debug.Log("玩家与门交互");
-            // TODO: 开门逻辑
+            if (!isOpen)
+            {
+                var newLimit = new JointAngleLimits2D();
+                newLimit.max = closeAngle;
+                newLimit.min = -openAngle;
+                hinge2D.limits = newLimit;
+                isOpen = true;
+                // 给一个向上的力
+                if (openAngle > 0)
+                {
+                    GetComponent<Rigidbody2D>().AddForce(0.5f * transform.up, ForceMode2D.Impulse);
+                }
+                else
+                {
+                    GetComponent<Rigidbody2D>().AddForce(-0.5f * transform.up, ForceMode2D.Impulse);
+                }
+            }
+            else
+            {
+                var newLimit = new JointAngleLimits2D();
+                newLimit.max = 0;
+                newLimit.min = 0;
+                hinge2D.limits = newLimit;
+                isOpen = false;
+            }
         }
 
         private void Update()
